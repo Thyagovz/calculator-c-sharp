14ab053dbc36a6fbf23df652b7e7ea46f6d995ba
Thyago Euclides <flooddk@gmail.com>
2026-06-29 09:48:28 -0400

    Add menu options for multiplication and division, and ensure return to menu after operations

--- Program.cs
+++ Program.cs
@@ -15,10 +15,21 @@ class Program
         Console.WriteLine("2 = Subtração");
         Console.WriteLine("3 = Multiplicação");
         Console.WriteLine("4 = Divisão");
+        Console.WriteLine("5 = Sair");
 
         Console.WriteLine("---------------");
         Console.WriteLine("Selecione uma opção: ");
         short res = short.Parse(Console.ReadLine());
+
+        switch (res)
+        {
+            case 1: Addition(); break;
+            case 2: Subtraction(); break;
+            case 3: Multiply(); break;
+            case 4: Division(); break;
+            case 5: System.Environment.Exit(0); break;
+            default: Menu(); break;
+        }
     }
     static void Addition()
     {
@@ -33,11 +44,11 @@ class Program
         float result = v1 + v2;
         Console.WriteLine($"O resultado da soma é {result}");
         Console.ReadKey();
+        Menu();
         // Console.WriteLine("O resultado da soma é " + resultado);
         // Console.WriteLine($"O resultado da soma é {v1 + v2}");
         // Console.WriteLine("O resultado da soma é " + (v1 + v2));
     }
-
     static void Subtraction()
     {
         Console.Clear();
@@ -53,8 +64,8 @@ class Program
         float result = v1 - v2;
         Console.WriteLine($"O resultado da subtração é {result}");
         Console.ReadKey();
+        Menu();
     }
-
     static void Division()
     {
         Console.Clear();
@@ -70,8 +81,8 @@ class Program
         float result = v1 / v2;
         Console.WriteLine($"O resultado da divisão é {result}");
         Console.ReadKey();
+        Menu();
     }
-
     static void Multiply()
     {
         Console.Clear();
@@ -87,6 +98,7 @@ class Program
         float result = v1 * v2;
         Console.WriteLine($"O resultado da divisão é {result}");
         Console.ReadKey();
+        Menu();
     }
 }
 
